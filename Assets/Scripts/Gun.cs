4c1d629dd1ad4f0151d57616706ebbd4fcea4343
FERGUDOM000 <FERGUDOM000@BGSD.k12.wa.us>
2020-02-04 07:49:47 -0800

    forgot to update

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10;
+    public float range = 100;
+
+    public Camera fpsCam;
+
+    public ParticleSystem muzzleFlash;
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+
+    }
+}
