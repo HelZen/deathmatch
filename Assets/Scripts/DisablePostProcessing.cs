ccd0efc64afa320dc315c7f67671e943e0500db0
FERGUDOM000 <FERGUDOM000@BGSD.k12.wa.us>
2020-02-04 07:46:25 -0800

    init commit

---
+++ Assets/Scripts/DisablePostProcessing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class DisablePostProcessing : MonoBehaviour
+{
+    public PostProcessLayer pp;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F9) && pp.enabled == false) {
+            pp.enabled = true;
+        } 
+        else if (Input.GetKeyDown(KeyCode.F9) && pp.enabled == true) {
+            pp.enabled = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/player/Grab.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    public Transform dest;
+    bool holding = false;
+    float speed = 25000f;
+    private void LateUpdate()
+    {
+        if (holding)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2 - 3f, Camera.main.nearClipPlane + 3f));
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Rigidbody>().AddForce(dest.transform.forward * speed * Time.deltaTime);
+                holding = false;
+                this.transform.parent = null;
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<Rigidbody>().freezeRotation = false;
+            }
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (holding == false)
+            {
+                if (Vector3.Distance(transform.position, dest.transform.position) <= 5)
+                {
+                    GetComponent<Rigidbody>().useGravity = false;
+                    holding = true;
+                    this.transform.parent = GameObject.Find("GrabDistance").transform;
+                    GetComponent<Rigidbody>().velocity = Vector3.zero;
+                    GetComponent<Rigidbody>().freezeRotation = true;
+                }
+            }
+            else
+            {
+                holding = false;
+                this.transform.parent = null;
+                GetComponent<Rigidbody>().useGravity = true;
+                GetComponent<Rigidbody>().freezeRotation = false;
+            }
+
+        }
+    }
+}
